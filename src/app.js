8b651c9bd7f132c0352ac6849ca0f605b262f4aa
OUSSAMA <msaadoussama1@gmail.com>
2026-02-13 01:25:56 +0100

    Implement CI/CD pipelines and Github actions

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import logger from '#config/logger.js';
-import helmet from "helmet";
+import helmet from 'helmet';
 import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
@@ -15,23 +15,37 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(morgan('combined', { stream: {
-  write: (message) => logger.info(message.trim()),
- }}));
+app.use(
+  morgan('combined', {
+    stream: {
+      write: message => logger.info(message.trim()),
+    },
+  })
+);
 
 app.get('/', (req, res) => {
   logger.info('Hello from Acquisition!');
   res.status(200).send('Hello, World!');
 });
 
-app.get('/health', (req, res) => {res.status(200).json({ status: 'OK', timestamp: new Date().toISOString() , uptime: process.uptime()});});
+app.get('/health', (req, res) => {
+  res
+    .status(200)
+    .json({
+      status: 'OK',
+      timestamp: new Date().toISOString(),
+      uptime: process.uptime(),
+    });
+});
 
-app.get('/api', (req, res) => {res.status(200).json({ message: 'Acquisition API is running!' });});
+app.get('/api', (req, res) => {
+  res.status(200).json({ message: 'Acquisition API is running!' });
+});
 app.use('/api/auth', authRoutes);
 app.use('/api/users', usersRoutes);
 
+app.use((req, res) => {
+  res.status(404).json({ error: 'Route Not Found' });
+});
+
 export default app;
-
-
-
-
--- src/config/arcjet.js
+++ src/config/arcjet.js
@@ -1,21 +1,17 @@
-import arcjet, { shield, detectBot, slidingWindow } from "@arcjet/node";
+import arcjet, { shield, detectBot, slidingWindow } from '@arcjet/node';
 
 const aj = arcjet({
   key: process.env.ARCJET_KEY,
   rules: [
-    shield({ mode: "LIVE" }),
+    shield({ mode: 'LIVE' }),
     detectBot({
-      mode: "LIVE",
-      allow: [
-        "CATEGORY:SEARCH_ENGINE",
-        "CATEGORY:PREVIEW",  
-        "CATEGORY:TOOL"
-      ],
+      mode: 'LIVE',
+      allow: ['CATEGORY:SEARCH_ENGINE', 'CATEGORY:PREVIEW', 'CATEGORY:TOOL'],
     }),
     slidingWindow({
-        mode: "LIVE",
-        interval: 60, // 1 minute
-        max: 5
+      mode: 'LIVE',
+      interval: 60, // 1 minute
+      max: 5,
     }),
   ],
 });
